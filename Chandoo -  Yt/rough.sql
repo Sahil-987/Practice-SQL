78ed02b23c09b62c0464785126b192c3b6dc9cf3
Sahil-987 <sahil83765@gmail.com>
2023-08-25 22:51:52 +0530

    deleted unnecessary folders

--- Chandoo -  Yt/rough.sql
+++ Chandoo -  Yt/rough.sql
@@ -1,1 +1,26 @@
+USE `awesome chocolates`;
+
 select * from sales;
+
+select * from people
+where Team = 'Jucies' or Team = 'Delish';
+
+
+select * from people
+where Team in ('Jucies','Delish');
+
+
+select * from people
+where Salesperson like 'D%';
+
+
+select SaleDate, 
+		Amount,
+		case when Amount < 1000 then 'under 1k'
+			when Amount < 5000 then 'under 5k'
+			when Amount <= 10000 then 'under 10k'
+		else 'greater than 10k'
+end as 'Amount Category'
+from sales;
+
+
