8143ce89f47ab8123eef73fb357caf18557a9f98
Sahil-987 <sahil83765@gmail.com>
2023-08-29 21:32:58 +0530

    practice - joins, group by

--- Chandoo -  Yt/rough.sql
+++ Chandoo -  Yt/rough.sql
@@ -24,3 +24,53 @@ end as 'Amount Category'
 from sales;
 
 
+-- Joins
+
+select s.SPID, s.GeoID, s.PID, s.Amount,
+	p.Salesperson
+	from sales s
+join people p
+on s.SPID = p.SPID;
+
+
+
+select s.SPID, p.SPID, pr.PID, 
+	s.Amount,
+	p.Salesperson
+ from
+sales s
+join people p 
+join products pr
+on s.SPID = p.SPID = pr.PID;
+
+
+-- Group by
+
+select PID,
+	avg(Amount) as mean_amount
+from
+sales
+group by PID
+order by mean_amount desc;
+
+
+select *,
+	year(SaleDate) as Year,
+    month(SaleDate) as Month
+ from 
+ sales;
+ 
+ 
+ select 
+	year(SaleDate) as Year,
+    month(SaleDate) as Month,
+    avg(Amount) as avg_monthly_sales
+ from 
+ sales
+ group by
+	year(SaleDate),
+	month(SaleDate)
+order by
+	avg_monthly_sales desc;
+
+
